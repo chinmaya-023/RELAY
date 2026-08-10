03f22736e09d50d5d5be0e327e314185fc540a14
chinmaya-023 <chinamyaghanashyamnayak@gmail.com>
2026-08-10 11:14:17 +0530

    Bug Fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: { extend: { colors: { relay: { 50: '#f0f7ff', 400: '#48a1ff', 500: '#1c7df2', 600: '#0964d8', 950: '#06172d' } } } },
+  theme: { extend: { colors: { relay: { 50: '#f0f7ff', 300: '#8fc5ff', 400: '#48a1ff', 500: '#1c7df2', 600: '#0964d8', 950: '#06172d' } } } },
   plugins: []
 };
